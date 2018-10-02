a7beb353b24cc30b122e8a5b41196921e8ced331
shirantha97 <39570216+shirantha97@users.noreply.github.com>
2018-10-02 23:31:47 +0530

    inventory

--- resources/views/inventory/supplier/live_search.blade.php
+++ resources/views/inventory/supplier/live_search.blade.php
@@ -1,5 +1,8 @@
 @extends('layout')
 @section('content')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <div class="container">
     <div class="container box">
         <h3 align="center"></h3><br/>
