e45a8311dca6d1bebfc4a513fe8b10e5c0bd93ad
rashmika97 <rashmikaopatha97@gmail.com>
2018-10-02 12:16:21 +0530

    sportShowEdit

--- resources/views/adminPages/allAdmins.blade.php
+++ resources/views/adminPages/allAdmins.blade.php
@@ -4,23 +4,28 @@
 
     <div class="col-12">
 
-    <table class="table-primary col-12" style="border: solid">
-        <tr  style="border: solid">
-            <th>Name</th>
-            <th>Email</th>
-            <th>Created On</th>
-            <th>Delete</th>
-        </tr>
+        <div>
 
-    @foreach($admins as $admin)
+            <table class="table-primary col-12" style="border: solid">
+                <div>
+                    <tr style="border: solid">
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Created On</th>
+                        <th>Delete</th>
+                    </tr>
+                </div>
+                @foreach($admins as $admin)
 
-        <tr  style="border:groove" >
-            <td>{{$admin->name}}</td>
-            <td>{{$admin->email}}</td>
-            <td>{{$admin->created_at}}</td>
-            <td> <button class="btn btn-danger" type="submit">
-                    <a class="text-white" href="/Admindestroy"
-                       onclick="
+                    <div>
+                        <tr style="border:groove;">
+                            <td>{{$admin->name}}</td>
+                            <td>{{$admin->email}}</td>
+                            <td>{{$admin->created_at}}</td>
+                            <td>
+                                <button class="btn btn-danger" type="submit">
+                                    <a class="text-white" href="/Admindestroy"
+                                       onclick="
                                                          var result = confirm('Are you sure youo want to delete this Event? ');
 
                                                          if(result){
@@ -29,23 +34,26 @@
                                                          }
 
                                                         "
-                    >
-                        Delete Event</a>
+                                    >
+                                        Delete Event</a>
 
-                    <form id="delete-form"
-                          action="{{action('CustomLoginController@Admindestroy', [$admin->id])}}"
-                          method="post" style="display:none">
-                        <input type="hidden" name="_method" value="delete">
-                        {{csrf_field()}}
-                    </form>
+                                    <form id="delete-form"
+                                          action="{{action('CustomLoginController@Admindestroy', [$admin->id])}}"
+                                          method="post" style="display:none">
+                                        <input type="hidden" name="_method" value="delete">
+                                        {{csrf_field()}}
+                                    </form>
 
-                </button> </td>
-        </tr>
+                                </button>
+                            </td>
+                        </tr>
 
-    @endforeach
+                    </div>
+                @endforeach
 
-    </table>
+            </table>
 
+        </div>
     </div>
 
 @endsection
